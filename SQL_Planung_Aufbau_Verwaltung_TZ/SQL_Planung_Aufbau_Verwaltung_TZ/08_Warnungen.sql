0804609accff0e02c47121d2b961f19014066836
Rauchendraser <andreasr@ppedv.de>
2021-12-03 12:23:00 +0100

    Create 08_Warnungen.sql

---
+++ SQL_Planung_Aufbau_Verwaltung_TZ/SQL_Planung_Aufbau_Verwaltung_TZ/08_Warnungen.sql
@@ -0,0 +1,23 @@
+
+
+--WARNUNGEN
+
+
+select from tabelle
+--Schweregrad 15 Erro 156
+
+
+select * from tabelle
+--Ebene 16  208
+
+
+--Meldung: 14-- zugriffsrechte fehlen
+--         15 / 16 Daufehler
+--         0 - 10  Infos kein Error
+--         17 oha
+-- bis 25  worst case
+
+--Wenn Ebene = 19 dann Mail
+
+
+select * t
